aaa6c7bcfd6c2dfe97a2337763e9bc6a14caa824
Jason <l>
2016-10-31 00:41:31 +0800

    remove packet

---
+++ TransferConcurrency.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace TransferConcurrency.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
